5a83daaad323c76fafb2223832ffa3b89805c86f
VolodyaBush <52610224+VolodyaBush@users.noreply.github.com>
2019-10-04 23:11:38 +0300

    Update object.cpp

--- object.cpp
+++ object.cpp
@@ -1,9 +1,5 @@
 #include "object.h"
 
-Object::Object(vector<Point> _points): points(_points) , color(0.0, 0.0, 0.0) {
+Object::Object(vector<Point> _points): points(_points) , color(0.0, 0.0, 0.0) {}
 
-}
-
-Object::Object(vector<Point> _points, const Color& _color): point(_points), color(_color) {
-
-}
+Object::Object(vector<Point> _points, const Color& _color): point(_points), color(_color) {}
